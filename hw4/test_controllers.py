c306f324f195ca6353c65c61d544abcf8feb382c
liapina-e <89636319125k@gmail.com>
2026-01-25 16:18:09 +0300

    Update test_controllers.py

--- hw4/test_controllers.py
+++ hw4/test_controllers.py
@@ -3,6 +3,6 @@ from .controllers import operation
 
 
 @pytest.mark.parametrize('a, b, expected', [(1, 2, 3), (5, -4, 1), (7, 8, 15)])
-def test_operation(a, b, expected):
+def test_operation(a: int, b: int, expected: int) -> None:
     received = operation(a, b)
     assert received == expected
